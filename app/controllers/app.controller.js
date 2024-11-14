348c42091fb44b0c035b5da8e33e7397860c35fd
Omar Badawy <omar.badawy@code.berlin>
2024-11-15 00:06:53 +0100

    update:filter updated

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -51,62 +51,13 @@ exports.getMain = async (req, res, next) => {
 
   exports.retrieveFiltered = (req, res, next) => {
     const {
-      body: { companyId, categories, offset = 0 },
+      body: { title, offset = 0 },
     } = req;
   
-    // prep categories filter
-    let filter;
-    try {
-      filter = JSON.parse(categories).map((elem) => ({ [Op.contains]: [elem] }));
-    } catch {
-      filter = null;
-    }
-  
-    if (companyId && filter) {
+    if (title) {
       return Advertisement.findAll({
-        where: {
-          [Op.and]: [
-            { companyId },
-            {
-              categories: {
-                [Op.or]: filter,
-              },
-            },
-          ],
-        },
+        where: { title },
         offset,
-        limit: 20,
-      })
-        .then((content) => {
-          res.status(200).json({
-            message: 'Contents retrieved',
-            data: content,
-          });
-        })
-        .catch((err) => next(err));
-    }
-    if (companyId) {
-      return Advertisement.findAll({
-        where: { companyId },
-        offset,
-        limit: 20,
-      })
-        .then((content) => {
-          res.status(200).json({
-            message: 'Contents retrieved',
-            data: content,
-          });
-        })
-        .catch((err) => next(err));
-    }
-    if (filter) {
-      return Advertisement.findAll({
-        where: {
-          categories: {
-            [Op.or]: filter,
-          },
-        },
-        offset: Number(offset),
         limit: 20,
       })
         .then((content) => {
