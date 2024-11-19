49a9789d7374e1d6d8f5a9e8ceb0e89492cf2bfb
Omar Badawy <omar.badawy@code.berlin>
2024-11-19 14:56:41 +0100

    bugfix/naming issue

---
+++ app/routes/campaign.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { isLoggedIn } = require('../middlewares/authorization.middleware');
+const campaign = require('../controllers/campaign.controller')
+
+
+router.get('/', isLoggedIn, campaign.getAllCampaigns);
+
+router.get('/:id', (req, res) => {
+  res.send(`Get advertisement with id: ${req.params.id}`);
+});
+
+router.post('/', isLoggedIn, campaign.createCampaign);
+
+router.put('/:id', isLoggedIn, campaign.updateCampaign);
+
+router.delete('/:id', isLoggedIn, campaign.deleteCampaign);
+
+router.patch('/:id/status', (req, res) => {
+  res.send(`Update status for campaign with id: ${req.params.id}`);
+});
+
+
+module.exports = router;
