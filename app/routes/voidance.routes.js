66d3c847dd9480870b76907d2e5e4097dbcc5284
Omar Badawy <omar.badawy@code.berlin>
2024-11-21 23:44:18 +0100

    ref:voidances routes

--- app/routes/voidance.routes.js
+++ app/routes/voidance.routes.js
@@ -3,41 +3,20 @@ const router = express.Router();
 const voidanceController = require('../controllers/voidance.controller');
 const { isLoggedIn } = require('../middlewares/authorization.middleware');
 
-// Voidance Invite Routes
 
-// Get all voidance invites
 router.get('/invites', isLoggedIn, voidanceController.getAllVoidanceInvites);
-
-// Get a specific voidance invite
-router.get('/invites/:id', isLoggedIn, voidanceController.getVoidanceInvite);
-
-// Create a new voidance invite
-router.post('/invites', isLoggedIn, voidanceController.createVoidanceInvite);
-
-// Delete a voidance invite
-router.delete('/invites/:id', isLoggedIn, voidanceController.deleteVoidanceInvite);
-
-// Update voidance invite acceptance status
-router.patch('/invites/:id', isLoggedIn, voidanceController.voidanceUpdateStatus);
-
-// Voidance Generated Routes
-
-// Get all generated voidances
 router.get('/generated', isLoggedIn, voidanceController.getAllGeneratedVoidances);
-
-// Get a specific generated voidance
+router.get('/invites/:id', isLoggedIn, voidanceController.getVoidanceInvite);
 router.get('/generated/:id', isLoggedIn, voidanceController.getGeneratedVoidance);
 
-// Create a new generated voidance
+router.post('/invites', isLoggedIn, voidanceController.createVoidanceInvite);
 router.post('/generated', isLoggedIn, voidanceController.createVoidance);
 
-// Delete a generated voidance
-router.delete('/generated/:id', isLoggedIn, voidanceController.deleteGeneratedVoidance);
-
-// Update generated voidance upload status
+router.patch('/invites/:id', isLoggedIn, voidanceController.voidanceUpdateStatus);
 router.patch('/generated/:id/upload-status', isLoggedIn, voidanceController.updateGeneratedVoidanceUploadStatus);
-
-// Update generated voidance quality score
 router.patch('/generated/:id/quality-score', isLoggedIn, voidanceController.updateGeneratedVoidanceQualityScore);
+
+router.delete('/generated/:id', isLoggedIn, voidanceController.deleteGeneratedVoidance);
+router.delete('/invites/:id', isLoggedIn, voidanceController.deleteVoidanceInvite);
 
 module.exports = router;
