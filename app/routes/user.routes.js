8848572e133e609d2f58a2d5a536d71ab431a3e0
Omar Badawy <omar.badawy@code.berlin>
2024-11-21 23:34:49 +0100

    ref:users routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,47 +5,21 @@ const user = require( '../controllers/user.controller.js');
 const session = require('../controllers/session.controller.js');
 const mailer = require('../controllers/mailer.controller.js');
 const { isLoggedIn } = require('../middlewares/authorization.middleware.js');
-const {
-  checkUsername,
-  checkEmail,
-} = require('../middlewares/validation.middleware.js');
+const { checkUsername, checkEmail,} = require('../middlewares/validation.middleware.js');
 
-// Post login request
-router.post('/login', passport.authenticate('local'), session.login);
-
-// Post logout request
-router.post('/logout', isLoggedIn, session.logout);
-
-// patch User Password
-router.patch('/updatePassword', isLoggedIn, user.updatePassword); 
-
-// Update a User with id
-router.patch('/:userName/update', isLoggedIn, user.updateOne);
-
-// Set resetPassword attributes & send resetPasswordMail
-router.post(
-  '/resetPassword',
-  user.setResetToken,
-  mailer.sendResetPasswordInstructions
-);
-
-// update User password & login
-router.post('/updatePassword/:token', user.replacePassword);
-
-// Validate session cookie
 router.get('/session', session.validateSession); 
-
-// Get User object
 router.get('/:userName', isLoggedIn, user.retrieveOne); 
 
-// express signup
-router.post(
-  '/signup',
-  checkUsername,
-  checkEmail,
-  user.expressSignup,
-  passport.authenticate('local'),
-  session.validationResponse
-);
+router.post('/login', passport.authenticate('local'), session.login);
+router.post('/logout', isLoggedIn, session.logout);
+router.post('/updatePassword/:token', user.replacePassword);
+router.post('/forgotPassword', user.setResetToken, mailer.sendResetPasswordInstructions);
+router.post('/signup', checkUsername, checkEmail, user.expressSignup, passport.authenticate('local'), session.validationResponse);
+
+router.patch('/updatePassword', isLoggedIn, user.updatePassword); 
+router.patch('/:userName/update', isLoggedIn, user.updateOne);
+
+
+
 
 module.exports = router;
