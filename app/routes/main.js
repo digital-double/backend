b080888792b7723cfe6a652353a3d6f5fa6bcd37
Omar Badawy <omar.badawy@code.berlin>
2024-11-21 23:48:23 +0100

    ref:main routes

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const router = express.Router();
-
 const userRoutes = require('./user.routes');
 const companyRoutes = require('./company.routes');
 const voidanceRoutes = require('./voidance.routes');
@@ -15,11 +13,6 @@ router.get('/', (_req, res) => {
   res.send('digital double');
 });
 
-router.post('/csp-reports', (req, res) => {
-  console.error(req.body);
-  res.status(204).end();
-});
-
 router.use('/app', appRoutes)
 router.use('/users', userRoutes);
 router.use('/companies', companyRoutes);
@@ -27,5 +20,10 @@ router.use('/advertisement',advertisementRoutes)
 router.use('/voidance', voidanceRoutes);
 router.use('/campaign', campaignRoutes);
 router.use('/instagram', instagramRoutes);
+
+router.post('/csp-reports', (req, res) => {
+  console.error(req.body);
+  res.status(204).end();
+});
 
 module.exports = router;
