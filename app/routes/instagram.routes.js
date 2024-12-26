1d24886878cc68e758b0b354188a70ac6d25959b
Omar Badawy <omar.badawy@code.berlin>
2024-12-26 19:53:22 +0100

    update: routes

--- app/routes/instagram.routes.js
+++ app/routes/instagram.routes.js
@@ -7,54 +7,16 @@ router.get('/credentials', (req, res) => {
   res.send('Get all Instagram credentials route');
 });
 
-
-router.get('/credentials/:id', (req, res) => {
-  res.send(`Get Instagram credentials with id: ${req.params.id}`);
-});
-
-
-router.post('/credentials', (req, res) => {
+router.post('/:id', (req, res) => {
   res.send('Create new Instagram credentials route');
 });
-
-
-router.put('/credentials/:id', (req, res) => {
-  res.send(`Update Instagram credentials with id: ${req.params.id}`);
-});
-
-
-router.delete('/credentials/:id', (req, res) => {
-  res.send(`Delete Instagram credentials with id: ${req.params.id}`);
-});
-
 
 router.patch('/credentials/:id/link', (req, res) => {
   res.send(`Link/Unlink Instagram account for credentials with id: ${req.params.id}`);
 });
 
-
-router.get('/content', (req, res) => {
-  res.send('Get all Instagram content route');
-});
-
-
-router.get('/content/:id', (req, res) => {
-  res.send(`Get Instagram content with id: ${req.params.id}`);
-});
-
-
-router.post('/content', (req, res) => {
-  res.send('Create new Instagram content route');
-});
-
-
-router.put('/content/:id', (req, res) => {
-  res.send(`Update Instagram content with id: ${req.params.id}`);
-});
-
-
-router.delete('/content/:id', (req, res) => {
-  res.send(`Delete Instagram content with id: ${req.params.id}`);
+router.delete('/credentials/:id', (req, res) => {
+  res.send(`Delete Instagram credentials with id: ${req.params.id}`);
 });
 
 module.exports = router;
